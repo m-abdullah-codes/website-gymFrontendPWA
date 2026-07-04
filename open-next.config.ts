c82cb8544bc94de2ea39cc045d0817df59c63009
m-abdullah-codes <abdullah.abid.ok@gmail.com>
2026-07-04 18:58:55 +0500

    cloudflarefix

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,8 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import staticAssetsIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache";
 
-export default defineCloudflareConfig({});
+// SSG routes: read prerendered cache from Workers Static Assets instead of the
+// default "dummy" store, which breaks ISR/revalidation on navigation.
+export default defineCloudflareConfig({
+  incrementalCache: staticAssetsIncrementalCache,
+});
